b34d443ec6a2956d4e35a97cfde20723242f9d64
lisibo <865474355@qq.com>
2017-07-05 19:07:13 +0800

    change drag 17

--- drag.js
+++ drag.js
@@ -20,6 +20,9 @@
  * git reset --hard HEAD^ 还原最新版本的前一个版本，内容会改变
  * git reset --hard HEAD`n 还原当前版本的前几个版本,内容会改变
  * git reflog 查看所有未来的commit号，可以通过 git reset --hard +(commit号) 回到新版本
+ * git remote 查询远程仓库的名字（默认origin）
+ * git remote add 修改远程仓库的名字
+ * 
  git diff 工作区与暂存区对比
  dit diff --cached 暂存区与版本库对比
  git diff master 工作区与版本库对比
