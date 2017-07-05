00c1b1d18a0ee339fd641061c1953683e7999496
lisibo <865474355@qq.com>
2017-07-05 18:43:25 +0800

    change drag 15

--- drag.js
+++ drag.js
@@ -17,7 +17,9 @@
  * git checkout -- 文件名 （工作区内容依次从暂存区还原内容，暂存区没有内容则直接还原版本区内容）
  * git checkout （commit号） +文件名   误删后还原指定版本的文件
  * git reset --hard （commit号） 还原版本 
- * 
+ * git reset --hard HEAD^ 还原最新版本的前一个版本，内容会改变
+ * git reset --hard HEAD`n 还原当前版本的前几个版本,内容会改变
+ * git reflog 查看所有未来的commit号，可以通过 git reset --hard +(commit号) 回到新版本
  git diff 工作区与暂存区对比
  dit diff --cached 暂存区与版本库对比
  git diff master 工作区与版本库对比
