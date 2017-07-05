2dca7fefaf6a296bd80561435a0096d5763ff094
lisibo <865474355@qq.com>
2017-07-05 17:55:11 +0800

    change drag 9

--- drag.js
+++ drag.js
@@ -6,10 +6,11 @@
  * git clone +url 克隆地址信息
  * git add +文件名 指定文件上传至暂存区
  * git add . 所有文件上传暂存区
+ * git commit -m "注释" （暂存区提交到版本库）
  * git commit -a -m “注释” 工作区上传的版本库
  * git reset HEAD +文件名 指定文件从暂存区返回工作区
  * git reset HEAD . 所有文件从暂存区返回工作区
- * git checkout -- 文件名 （工作区内容还原到版本库内容）
+ * git checkout -- 文件名 （工作区内容依次从暂存区还原内容，暂存区没有内容直接还原版本区内容）
  git diff 工作区与暂存区对比
  dit diff --cached 暂存区与版本库对比
  git diff master 工作区与版本库对比
